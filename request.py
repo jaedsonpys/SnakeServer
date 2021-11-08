2141d35ecdc84c897b9272a1922223e7878e8595
Jaedson Silva <contacanal798@gmail.com>
2021-11-08 16:02:06 -0300

    removendo funções desnecessárias e utilizando a função .get() em request.py

--- request.py
+++ request.py
@@ -27,67 +27,25 @@ class Request:
         # Os cookies são obtidos assim que a requisição
         # começa a ser processada. #
 
-        self.__cookies = {}
+        self.cookies = {}
 
         if 'COOKIE' in self.headers.keys():
-            __cookies_list = str(self.headers['COOKIE']).split(';')
+            cookies_list = str(self.headers['COOKIE']).split(';')
             
-            for i in __cookies_list:
+            for i in cookies_list:
                 __cookie = str(i).replace(' ', '').split('=')
-                self.__cookies[__cookie[0]] = __cookie[1]
-
-
-    def get_header(self, key: str):
-        '''Obtém um header através
-        do seu nome.
-        
-        :param key: Nome do header'''
-
-        # Função para disponibilizar os cookies
-        # para o usuário.
-
-        if key in self.headers.keys():
-            return self.headers[key]
-        else:
-            return None
+                self.cookies[__cookie[0]] = __cookie[1]
 
 
     def __query(self):
         # Essa função obtém todos os parâmetros de URL
         # e os guarda em um objeto. #
         
-        self.__querys = {}
+        self.params = {}
 
         if not len(self.query) == 0:
             __query_list = str(self.query).split('&')
 
             for i in __query_list:
                 __query = i.split('=')
-                self.__querys[__query[0]] = __query[1]
-
-
-    def get_query(self, name: str):
-        '''Obtém uma query (ou parâmetro) da URL através do
-        seu nome:
-        
-        :param key: Nome da query'''
-
-        if name in self.querys:
-            return URL().decode(self.querys[name])
-        else:
-            return None
-
-
-    def get_cookie(self, key: str):
-        ''''Obtém o valor de um cookie
-        através do seu nome.
-
-        Se o valor retornado for None, o cookie
-        não existe.
-        
-        :param key: Nome do cookie'''
-
-        if key in self.cookies:
-            return self.cookies[key]
-        else:
-            return None
+                self.params[__query[0]] = __query[1]
